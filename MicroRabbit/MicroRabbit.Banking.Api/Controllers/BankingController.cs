031db3fe9efb385e698608f8e95cc9e0ab992402
ajith K <kamathajithk2012@gmail.com>
2020-06-08 22:46:16 +0530

    Completed Banking Microservice

---
+++ MicroRabbit/MicroRabbit.Banking.Api/Controllers/BankingController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MicroRabbit.Banking.Application.Interfaces;
+using MicroRabbit.Banking.Domain.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace MicroRabbit.Banking.Api.Controllers
+{
+	[ApiController]
+	[Route("api/[controller]")]
+	public class BankingController : ControllerBase
+	{
+		private readonly IAccountService _accServ;
+		private static readonly string[] Summaries = new[]
+		{
+			"Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+		};
+
+		private readonly ILogger<BankingController> _logger;
+
+		public BankingController(ILogger<BankingController> logger , IAccountService accountService)
+		{
+			_logger = logger;
+			_accServ = accountService;
+		}
+
+		[HttpGet]
+		public IEnumerable<Account> Get()
+		{
+			return _accServ.GetAccounts();
+		}
+	}
+}
---
+++ MicroRabbit/MicroRabbit.Banking.Data/Context/BankingDBContext.cs
@@ -0,0 +1,30 @@
+﻿using MicroRabbit.Banking.Domain.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MicroRabbit.Banking.Data.Context
+{
+	public class BankingDBContext : DbContext
+	{
+		public BankingDBContext(DbContextOptions options) : base(options)
+		{
+
+		}
+
+		public DbSet<Account> Accounts { get; set; }
+	}
+
+	public class BankingFactory : IDesignTimeDbContextFactory<BankingDBContext>
+	{
+		public BankingDBContext CreateDbContext(string[] args)
+		{
+			var optionsBuilder = new DbContextOptionsBuilder<BankingDBContext>();
+			optionsBuilder.UseSqlServer("Server= LAPTOP-DEAN8LG7\\SQLEXPRESS; Database = BankingDB;Trusted_Connection=true; MultipleActiveResultSets= true");
+
+			return new BankingDBContext(optionsBuilder.Options);
+		}
+	}
+}
